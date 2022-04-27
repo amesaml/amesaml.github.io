a3c8af0e2b91ec3998fbac6bfcdbb997a66c8b70
amesaml <102702045+amesaml@users.noreply.github.com>
2022-04-27 12:25:14 -0600

    Add files via upload

---
+++ WildRescues/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: Sam Ames
+    File Name: script.js
+    Date: 04/27/2022
+*/
+
+// jQuery for hero image to consume the header window space //
+
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
